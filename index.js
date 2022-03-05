8777b38c5bc981e189f1172189e85d509a00f6ea
Arctur <mihailo.dilparic@gmail.com>
2022-03-05 14:48:36 +0100

    Added function to change divider based on screen size

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const adviceNumber = document.querySelector('.adviceNumber');
 const adviceText = document.querySelector('.adviceText');
 
+let x = window.matchMedia("(min-width: 1440px)");
 // Fetch some when the window is loaded
 window.onload = function () {
+    setDivider(x);
     fetchAdvice();
+    x.addEventListener("change", setDivider)
 }
 
+// Function to fetch advice with API and set textContent of elements 
 function fetchAdvice() {
     fetch("https://api.adviceslip.com/advice", {
         cache: "no-cache"
@@ -16,4 +20,14 @@ function fetchAdvice() {
         adviceNumber.textContent = adviceNumberContent;
         adviceText.textContent = adviceTextContent;
     })
+}
+
+// Function for changing the divider based on screen size
+function setDivider(screenSize) {
+    let divider = document.getElementById('divider');
+    if (screenSize.matches) {
+        divider.src = "pattern-divider-desktop.svg";
+    } else {
+        divider.src = "pattern-divider-mobile.svg";
+    }
 }
