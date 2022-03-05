1cd3d28270a67d6b9b444001a4eed206fdb4c2a2
Arctur <mihailo.dilparic@gmail.com>
2022-03-05 14:16:13 +0100

    Removed caching for fetch request, added function to fetch advice on page load
    Cache was removed so that a new request is made with each click

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const adviceNumber = document.querySelector('.adviceNumber');
 const adviceText = document.querySelector('.adviceText');
 
-document.onload()
+// Fetch some when the window is loaded
+window.onload = function () {
+    fetchAdvice();
+}
 
 function fetchAdvice() {
-    fetch("https://api.adviceslip.com/advice").then(response => response.json()).then(data => {
+    fetch("https://api.adviceslip.com/advice", {
+        cache: "no-cache"
+    }).then(response => response.json()).then(data => {
         let adviceNumberContent = `Advice #${data.slip.id}`;
-        let adviceTextContent = data.slip.advice;
+        let adviceTextContent = `“${data.slip.advice}“`;
 
         adviceNumber.textContent = adviceNumberContent;
         adviceText.textContent = adviceTextContent;
